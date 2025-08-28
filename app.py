b7a852fcbf436a688d7e05dff507feb886f9cf25
Lucas Emanuel <168912467+LucasDoJava@users.noreply.github.com>
2025-08-28 20:45:36 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,36 +1,36 @@
 from Helpers.app import app, api
 from Helpers.database import db
-from Helpers.CORS import cors
 from flask_cors import CORS
-
-CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "http://127.0.0.1:3000"]}})
 
 from Models import uf, mesorregiao, microrregiao, municipio, instituicao
 
-from Resources.InstituicaoResource import InstituicoesResouce, NovaInstituicaoResouce, InstituicaoResouce
-from Resources.UfResource import UfsResouce, UfResource
-from Resources.MesorregiaoResource import MesorregioesResouce, MesorregiaoResource
-from Resources.MicrorregiaoResource import MicrorregioesResouce, MicrorregiaoResource
-from Resources.MunicipioResource import MunicipiosResouce, MunicipioResource
+CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "http://127.0.0.1:3000"]}})
 
-from Resources.MatriculaResource import MatriculasPorRegiaoResource, MatriculasPorUFResource
+# Resources
+from Resources.UfResource import UfsResouce
+from Resources.InstituicaoResource import InstituicoesResouce
+from Resources.MesorregiaoResource import MesorregioesResouce
+from Resources.MicrorregiaoResource import MicrorregioesResouce
+from Resources.MunicipioResource import MunicipiosResouce
+from Resources.MatriculaResource import (
+    MatriculasPorUFResource,
+    MatriculasPorRegiaoResource
+)
+
+# Rotas
+api.add_resource(UfsResouce, "/ufs")
+
+api.add_resource(
+    InstituicoesResouce,
+    "/instituicoes",            
+    "/instituicoes/<int:ano>",  
+)
+
+api.add_resource(MesorregioesResouce, "/mesorregioes")
+api.add_resource(MicrorregioesResouce, "/microrregioes")
+api.add_resource(MunicipiosResouce, "/municipios")
 
 
-api.add_resource(UfsResouce, '/ufs')
-api.add_resource(UfResource, '/uf/<int:coduf>')
-
-api.add_resource(MesorregioesResouce, '/mesorregioes')
-api.add_resource(MesorregiaoResource, '/mesorregiao/<int:codmesorregiao>')
-
-api.add_resource(MicrorregioesResouce, '/microrregioes')
-api.add_resource(MicrorregiaoResource, '/microrregiao/<int:codmicrorregiao>')
-
-api.add_resource(MunicipiosResouce, '/municipios')
-api.add_resource(MunicipioResource, '/municipio/<int:codmunicipio>')
-
-api.add_resource(NovaInstituicaoResouce, '/instituicoes')
-api.add_resource(InstituicoesResouce, '/instituicoes/<int:ano>')
-api.add_resource(InstituicaoResouce, '/instituicoes/<int:ano>/<int:codentidade>')
-
-api.add_resource(MatriculasPorUFResource, "/instituicoes/<int:ano>/matriculas/uf")
-api.add_resource(MatriculasPorRegiaoResource, "/instituicoes/<int:ano>/matriculas/regiao")
+                  
+api.add_resource(MatriculasPorUFResource, "/matriculas/uf/<int:ano>")    
+api.add_resource(MatriculasPorRegiaoResource, "/matriculas/regiao/<int:ano>")  
